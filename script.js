d0b0a5b096648144b62de8e58124969fbf931fc1
Sid131313 <kolomenskiydanil@gmail.com>
2026-01-16 14:59:23 +0300

    feedback

---
+++ script.js
@@ -0,0 +1,21 @@
+const faqItems = document.querySelectorAll('.faq__item');
+
+  faqItems.forEach(item => {
+    const question = item.querySelector('.faq__question');
+    const answer = item.querySelector('.faq__answer');
+
+    question.addEventListener('click', () => {
+      const isActive = item.classList.contains('active');
+
+      faqItems.forEach(i => {
+        i.classList.remove('active');
+        i.querySelector('.faq__answer').style.maxHeight = null;
+      });
+
+      if (!isActive) {
+        item.classList.add('active');
+        answer.style.maxHeight = answer.scrollHeight + 'px';
+      }
+    });
+  });
+
